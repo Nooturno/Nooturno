04d9a15a877bbd3cf50ec4450a08d3e78e710701
Nooturno <raisonery@gmail.com>
2024-10-26 17:35:46 -0300

    Update Noot-Games-List.lua

--- Noot-Games-List.lua
+++ Noot-Games-List.lua
@@ -1,6 +1,6 @@
 local Games = {
 
-    [383310974] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/Games/Adopt-Me",
+    [383310974] = "https://raw.githubusercontent.com/Nooturno/Nooturno/refs/heads/main/Noot-Games-List.lua",
 }
 
 return Games
